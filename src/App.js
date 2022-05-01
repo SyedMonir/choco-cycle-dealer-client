5a048881c392a34b0e50606253a803d1cdf31047
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-05-01 14:47:11 +0600

    added hero section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home';
 import Header from './Pages/SharedComponent/Header/Header';
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
     </>
   );
 }
---
+++ src/Pages/Home/HeroSection/HeroSection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HeroSection = () => {
+  return (
+    <div className="">
+      <img
+        className="w-full  h-[90%]"
+        src="https://i.ibb.co/hydzDRs/Blue-Bycycle-Review-Thumbnail.gif"
+        alt="Hero"
+      />
+    </div>
+  );
+};
+
+export default HeroSection;
