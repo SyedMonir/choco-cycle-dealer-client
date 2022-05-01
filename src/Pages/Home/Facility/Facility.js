7319318b918e2ddff77e9e001ac89ce1db5f1c3e
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-05-02 04:15:35 +0600

    added aos-animation and add animation in facility section

--- src/Pages/Home/Facility/Facility.js
+++ src/Pages/Home/Facility/Facility.js
@@ -1,28 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import AOS from 'aos';
 
 const Facility = () => {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
   return (
     <section className="text-center py-12 text-gray-300 bg-[#1f4037]">
       <p className="text-xs">YOUR RIDE START HERE.</p>
       <h2 className="text-3xl text-[#88f5c2]">Our Facilities and Features</h2>
-      <span class="inline-block h-1 w-16 rounded bg-[#88f5c2] mt-6 mb-4"></span>
+      <span className="inline-block h-1 w-16 rounded bg-[#88f5c2] mt-6 mb-4"></span>
 
-      <section class="  body-font">
-        <div class="container px-5 py-10 mx-auto">
-          <div class="flex flex-wrap -m-4">
-            <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
-              <div class="h-full text-center">
+      <section className="body-font">
+        <div className="container px-5 py-10 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            <div
+              data-aos="zoom-in-right"
+              data-aos-delay="80"
+              data-aos-duration="1000"
+              data-aos-easing="ease-in-out"
+              className="lg:w-1/3 lg:mb-0 mb-6 p-4"
+            >
+              <div className="h-full text-center">
                 <img
                   alt="testimonial"
-                  class="w-24 h-24 mb-8 p-2 object-cover object-center rounded-full inline-block border-2 border-gray-800 bg-gray-800 bg-opacity-10"
+                  className="w-24 h-24 mb-8 p-2 object-cover object-center rounded-full inline-block border-2 border-gray-800 bg-gray-800 bg-opacity-10"
                   src="https://i.ibb.co/VJJ7f7w/unicycle.png"
                 />
 
-                <h2 class="text-white font-medium title-font tracking-wider text-sm">
+                <h2 className="text-white font-medium title-font tracking-wider text-sm">
                   Complete Overhaul
                 </h2>
-                <span class="inline-block h-1 w-10 rounded bg-[#88f5c2] mt-6 mb-4"></span>
-                <p class="leading-relaxed">
+                <span className="inline-block h-1 w-10 rounded bg-[#88f5c2] mt-6 mb-4"></span>
+                <p className="leading-relaxed">
                   An overhaul is a comprehensive service during which the
                   complete bike is disassembled, bearings cleaned or replaced,
                   drivetrain cleaned and cables and housing replaced. After
@@ -30,18 +41,24 @@ const Facility = () => {
                 </p>
               </div>
             </div>
-            <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
-              <div class="h-full text-center">
+            <div
+              data-aos="zoom-in"
+              data-aos-delay="80"
+              data-aos-duration="1000"
+              data-aos-easing="ease-in-out"
+              className="lg:w-1/3 lg:mb-0 mb-6 p-4"
+            >
+              <div className="h-full text-center">
                 <img
                   alt="testimonial"
-                  class="w-24 h-24 mb-8 p-2 object-cover object-center rounded-full inline-block border-2 border-gray-800 bg-gray-800 bg-opacity-10"
+                  className="w-24 h-24 mb-8 p-2 object-cover object-center rounded-full inline-block border-2 border-gray-800 bg-gray-800 bg-opacity-10"
                   src="https://i.ibb.co/t8NSPXf/helmet.png"
                 />
-                <h2 class="text-white font-medium title-font tracking-wider text-sm">
+                <h2 className="text-white font-medium title-font tracking-wider text-sm">
                   Custom Parts & Accessories
                 </h2>
-                <span class="inline-block h-1 w-10 rounded bg-[#88f5c2] mt-6 mb-4"></span>
-                <p class="leading-relaxed">
+                <span className="inline-block h-1 w-10 rounded bg-[#88f5c2] mt-6 mb-4"></span>
+                <p className="leading-relaxed">
                   Custom Junkies offers bike parts and components for classic
                   (steel) road bikes, single speed and track bikes, fixed gear
                   and MTB. Our goal is to offer not only a fine selection of
@@ -52,18 +69,24 @@ const Facility = () => {
                 </p>
               </div>
             </div>
-            <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
-              <div class="h-full text-center">
+            <div
+              data-aos="zoom-in-left"
+              data-aos-delay="80"
+              data-aos-duration="1000"
+              data-aos-easing="ease-in-out"
+              className="lg:w-1/3 lg:mb-0 mb-6 p-4"
+            >
+              <div className="h-full text-center">
                 <img
                   alt="testimonial"
-                  class="w-24 h-24 mb-8 p-2 object-cover object-center rounded-full inline-block border-2 border-gray-800 bg-gray-800 bg-opacity-10"
+                  className="w-24 h-24 mb-8 p-2 object-cover object-center rounded-full inline-block border-2 border-gray-800 bg-gray-800 bg-opacity-10"
                   src="https://i.ibb.co/w7Hnttm/bicycle.png"
                 />
-                <h2 class="text-white font-medium title-font tracking-wider text-sm">
+                <h2 className="text-white font-medium title-font tracking-wider text-sm">
                   Bike Fitting & Delivery
                 </h2>
-                <span class="inline-block h-1 w-10 rounded bg-[#88f5c2] mt-6 mb-4"></span>
-                <p class="leading-relaxed">
+                <span className="inline-block h-1 w-10 rounded bg-[#88f5c2] mt-6 mb-4"></span>
+                <p className="leading-relaxed">
                   A Complete Fit will measure your body height, upper body, leg
                   & arm length, shoulder width and foot size on our 2D Body
                   Analyzer. Based on those metrics, our unique algorithm, your
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'tw-elements';
+// AOS Animation
+import 'aos/dist/aos.css';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
